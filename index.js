6abbd01986fef9f044be4691c34871b17c98b89b
Juan Augusto <107640040+JuanAug@users.noreply.github.com>
2022-07-14 12:28:22 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const valorSistema = 850.75;
+
+const modulos = [
+    { id: 1, name: "oficina", valor: 50.0 },
+    { id: 2, name: "vendas", valor: 50.0 },
+    { id: 3, name: "financeiro", valor: 75.0 },
+    { id: 4, name: "NFe", valor: 125.0 },
+    { id: 5, name: "NFCe", valor: 125.0 },
+    { id: 6, name: "NFSe", valor: 155.15 },
+    { id: 7, name: "Estoque online", valor: 175.0 }
+]
+
+modulos.push({id: 3})
+console.log(modulos)
